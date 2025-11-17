03248f0a6c4991ddcb10df08e8305037a27ce861
Patrick Jiang <patjiang04@gmail.com>
2025-11-17 12:06:03 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,9 @@ import platform
 from synset_label import labels
 from rknnlite.api import RKNNLite
 
-# decice tree for RK356x/RK3576/RK3588
 DEVICE_COMPATIBLE_NODE = '/proc/device-tree/compatible'
 
 def get_host():
-    # get platform and device type
     system = platform.system()
     machine = platform.machine()
     os_machine = system + '-' + machine
@@ -18,12 +16,6 @@ def get_host():
                 device_compatible_str = f.read()
                 if 'rk3562' in device_compatible_str:
                     host = 'RK3562'
-                elif 'rk3576' in device_compatible_str:
-                    host = 'RK3576'
-                elif 'rk3588' in device_compatible_str:
-                    host = 'RK3588'
-                else:
-                    host = 'RK3566_RK3568'
         except IOError:
             print('Read device node {} failed.'.format(DEVICE_COMPATIBLE_NODE))
             exit(-1)
@@ -32,18 +24,12 @@ def get_host():
     return host
 
 INPUT_SIZE = 224
-
-RK3566_RK3568_RKNN_MODEL = 'resnet18_for_rk3566_rk3568.rknn'
-RK3588_RKNN_MODEL = 'resnet18_for_rk3588.rknn'
 RK3562_RKNN_MODEL = 'resnet18_for_rk3562.rknn'
-RK3576_RKNN_MODEL = 'resnet18_for_rk3576.rknn'
 
 
 def show_top5(result):
     output = result[0].reshape(-1)
-    # Softmax
     output = np.exp(output) / np.sum(np.exp(output))
-    # Get the indices of the top 5 largest values
     output_sorted_indices = np.argsort(output)[::-1][:5]
     top5_str = 'resnet18\n-----TOP 5-----\n'
     for i, index in enumerate(output_sorted_indices):
@@ -57,24 +43,14 @@ def show_top5(result):
 
 
 if __name__ == '__main__':
-
-    # Get device information
     host_name = get_host()
-    if host_name == 'RK3566_RK3568':
-        rknn_model = RK3566_RK3568_RKNN_MODEL
-    elif host_name == 'RK3562':
+    if host_name == 'RK3562':
         rknn_model = RK3562_RKNN_MODEL
-    elif host_name == 'RK3576':
-        rknn_model = RK3576_RKNN_MODEL
-    elif host_name == 'RK3588':
-        rknn_model = RK3588_RKNN_MODEL
     else:
         print("This demo cannot run on the current platform: {}".format(host_name))
         exit(-1)
 
     rknn_lite = RKNNLite()
-
-    # Load RKNN model
     print('--> Load RKNN model')
     ret = rknn_lite.load_rknn(rknn_model)
     if ret != 0:
@@ -85,25 +61,14 @@ if __name__ == '__main__':
     ori_img = cv2.imread('./space_shuttle_224.jpg')
     img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)
     img = np.expand_dims(img, 0)
-
-    # Init runtime environment
     print('--> Init runtime environment')
-    # Run on RK356x / RK3576 / RK3588 with Debian OS, do not need specify target.
-    if host_name in ['RK3576', 'RK3588']:
-        # For RK3576 / RK3588, specify which NPU core the model runs on through the core_mask parameter.
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0)
-    else:
-        ret = rknn_lite.init_runtime()
+    ret = rknn_lite.init_runtime()
     if ret != 0:
         print('Init runtime environment failed')
         exit(ret)
     print('done')
-
-    # Inference
     print('--> Running model')
     outputs = rknn_lite.inference(inputs=[img])
-
-    # Show the classification results
     show_top5(outputs)
     print('done')
 
